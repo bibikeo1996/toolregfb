4f172e4e56843b71a6bc1a74a57f0189f8b6eae2
ThienBui1801 <buiquythien1801@gmail.com>
2024-12-18 06:42:45 +0700

    add delete cache

--- include/deleteApp.py
+++ include/deleteApp.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 
+
 def is_app_installed(package_name):
     """
     Kiểm tra xem app có được cài đặt không thông qua lệnh ADB.
@@ -12,9 +13,18 @@ def is_app_installed(package_name):
 def xoa_app(adb_path, package_name):
     try:
         # Kiểm tra xem ứng dụng có được cài đặt không
-        if not is_app_installed(package_name):  # Sửa lỗi ở đây
+        if not is_app_installed(package_name):
             print(f"Ứng dụng {package_name} không được cài đặt trên BlueStacks.")
             return
+
+        # Xóa dữ liệu và cache trước khi gỡ cài đặt
+        print(f"Đang xóa cache và dữ liệu ứng dụng {package_name}...")
+        subprocess.run(
+            [adb_path, "shell", "pm", "clear", package_name],
+            capture_output=True,
+            text=True,
+        )
+        print(f"Đã xóa cache và dữ liệu ứng dụng {package_name} thành công")
 
         # Thực hiện lệnh gỡ cài đặt
         print(f"Đang gỡ cài đặt ứng dụng {package_name}...")
@@ -29,4 +39,3 @@ def xoa_app(adb_path, package_name):
             print(f"Lỗi khi gỡ cài đặt ứng dụng {package_name}: {result.stderr}")
     except Exception as e:
         print(f"Không thể gỡ cài đặt ứng dụng {package_name}. Lỗi: {e}")
-
