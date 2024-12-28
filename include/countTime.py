46e6849a11672896290fab65d7b493ef9d80f1d1
Joe Pham <joe.pham@patroids.com>
2024-12-28 11:03:11 +0700

    Update Remove source

--- include/countTime.py
+++
@@ -1,9 +0,0 @@
-import time
-import sys
-
-def demthoigian(seconds):
-    for i in range(1, seconds + 1):
-        sys.stdout.write('\r' + str(i))
-        sys.stdout.flush()
-        time.sleep(1)
-    sys.stdout.write('\n')    
