38fa6ea5db98c3a918d155bf2f4d219eb831b52e
ThienBui1801 <buiquythien1801@gmail.com>
2025-01-05 20:58:51 +0700

    u

--- include/run.py
+++ include/run.py
@@ -45,37 +45,37 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
     # mi = data["mi"]
     # phpsessid = data["phpsessid"]
 
-    emailText = "kavif45498@myweblaw.com"
+    emailText = "dofafe9582@pixdd.com"
     passText = "9dVhsUax@"
-    fieldFirstName = "Yen"
-    fieldLastName = "Pham"
+    fieldFirstName = "Tony"
+    fieldLastName = "Nguyen"
     # mi = data["mi"]
     # phpsessid = data["phpsessid"]
     # print(f"Email: {emailText} - Pass: {passText} - First Name: {fieldFirstName} - Last Name: {fieldLastName} - MI: {mi} - PHPSESSID: {phpsessid}")
 
     # RebootVaXoaCache(index, apk_path, package_name, ld_path_console, ld_path_instance)
     # quit
-    # isSetup = ThietLapThongSoThietbi(index, ld_path_console)
-    # if(isSetup == True):
-    #     pass
+    isSetup = ThietLapThongSoThietbi(index, ld_path_console)
+    if(isSetup == True):
+        pass
 
-    # isStarted = KhoiDongLDPlayer(index, ld_path_console)
-    # if(isStarted == True):
-    #     pass
+    isStarted = KhoiDongLDPlayer(index, ld_path_console)
+    if(isStarted == True):
+        pass
 
-    # isInstalled = KiemTraDaCaiAppFaceBookLiteChua(index, package_name, apk_path, ld_path_console)
-    # if(isInstalled == True):
-    #     pass
+    isInstalled = KiemTraDaCaiAppFaceBookLiteChua(index, package_name, apk_path, ld_path_console)
+    if(isInstalled == True):
+        pass
 
-    # DemThoiGian(1)
+    DemThoiGian(1)
 
-    # CapQuyenTruyCapChoFacebookLite(index, ld_path_console, package_name)
+    CapQuyenTruyCapChoFacebookLite(index, ld_path_console, package_name)
 
-    # DemThoiGian(2)
+    DemThoiGian(2)
 
     OpenApp(index)
     
-    # DemThoiGian(5)
+    DemThoiGian(5)
 
     # Tạo các flag để kiểm soát từng điều kiện
     createbutton_done = False
@@ -98,6 +98,7 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
     passwordField_done = False
     agree_done = False
     issue282_done = False
+    okbtn_done = False
 
     saveText = {}
     while True:
@@ -137,7 +138,7 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
 
         # Kiểm tra selectyourname nếu chưa hoàn thành
         if not selectyourname_done:
-            pos = TimAnhSauKhiChupVaSoSanh(Action.selectyourname_Btn, index, ld_path_console, max_attempts=5, check_attempt=True)
+            pos = TimAnhSauKhiChupVaSoSanh(Action.selectyourname_Btn, index, ld_path_console, max_attempts=2, check_attempt=True)
             if pos is not None:
                 Tap(index, ld_path_console, pos[0], pos[1])
                 selectyourname_done = True
@@ -196,7 +197,7 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
         if not doyouhaveaccount_done:
             ### Chỗ này có max_attempts để tăng số lần kiểm tra Element (hiện tại là 1)
             ### Check_attempt=True là cho phép kiểm tra element
-            pos = TimAnhSauKhiChupVaSoSanh(Action.doyouhaveaccount_Btn, index, ld_path_console, max_attempts=1, check_attempt=True)
+            pos = TimAnhSauKhiChupVaSoSanh(Action.doyouhaveaccount_Btn, index, ld_path_console, max_attempts=2, check_attempt=True)
             if pos is not None:
                 pos = TimAnhSauKhiChupVaSoSanh(Action.continuecreate_Btn, index, ld_path_console)
                 if pos is not None:
@@ -232,7 +233,7 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
 
         ## Kiểm tra có bị dính issue 282 nếu có thì reboot xóa cache chạy lại
         if not issue282_done:
-            is282 = TimAnhSauKhiChupVaSoSanh(Action.issue282_Btn, index, ld_path_console, max_attempts=2, check_attempt=True)
+            is282 = TimAnhSauKhiChupVaSoSanh(Action.issue282_Btn, index, ld_path_console, max_attempts=5, check_attempt=True)
             if(is282 != None):
                 issue282_done = True
                 print(f"Email: {emailText} bị dính 282")
@@ -244,14 +245,21 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
             pos = TimAnhSauKhiChupVaSoSanh(Action.verifycodefield_Btn, index, ld_path_console)
             if pos is not None:
 
-                ## Chỗ này phải đảm bảo verify code đã được lấy mới chạy tiếp
-                # verifycode = getMailCode(mi, phpsessid)
-                # if verifycode is None:
-                #     print("Không lấy được mã code == Reboot và xóa cache")
-                #     # RebootVaXoaCache(index, apk_path, package_name, ld_path_console, ld_path_instance)
-                #     return
+                # Chỗ này phải đảm bảo verify code đã được lấy mới chạy tiếp
+                verifycode = getMailCode(mi, phpsessid)
+                if verifycode is None:
+                    print("Không lấy được mã code == Reboot và xóa cache")
+                    RebootVaXoaCache(index, apk_path, package_name, ld_path_console, ld_path_instance)
+                    return
                 GoText(index, ld_path_console, verifycode, pos[0], pos[1])
+                DemThoiGian(30)
                 verifycode_done = True
+        
+        if not okbtn_done:
+            pos = TimAnhSauKhiChupVaSoSanh(Action.ok_Btn, index, ld_path_console)
+            if(pos != None):
+                Tap(index, ld_path_console, pos[0], pos[1])   
+                okbtn_done = True 
 
         if XuLyNextButton(index, ld_path_console, Action.skip_Btn):
             print("Đã click skip")
@@ -285,5 +293,5 @@ def RunLD(index, apk_path, package_name, ld_path_console, ld_path_instance):
         # Nếu tất cả các điều kiện đã hoàn thành, thoát khỏi vòng lặp
         if (createbutton_done and getstarted_done and firstname_done and lastname_done and nextt_done and
             selectyourname_done and setDate_done and sett_done and gender_done and signup_done and email_done and
-            doyouhaveaccount_done and password_done and verifycode_done and successReg_done and notnow_done and passwordField_done and agree_done and issue282_done):
+            doyouhaveaccount_done and password_done and verifycode_done and successReg_done and notnow_done and passwordField_done and agree_done and issue282_done and okbtn_done):
             break
