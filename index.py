020d252b95fc42e8cba436735c1c7e67251530a9
Joe Pham <joe.pham@patroids.com>
2025-01-07 15:15:50 +0700

    u

--- index.py
+++ index.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
                 t = threading.Thread(target=task, args=(i, apk_path, package_name, ld_path_console, ld_path_instance))
                 threads.append(t)
                 t.start()
-                time.sleep(2)  # Delay of 1 second before starting the next thread
+                time.sleep(5)  # Delay of 1 second before starting the next thread
 
             for t in threads:
                 t.join()
